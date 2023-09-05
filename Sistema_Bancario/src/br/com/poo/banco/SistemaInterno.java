5d502432355c61c3821b82465694e7b13a2ddb4d
Felipe Sutter <lipeps61@gmail.com>
2023-09-05 12:57:27 -0300

    feat: criação dos atributos get set e construtores da classe Funcionario

--- Sistema_Bancario/src/br/com/poo/banco/SistemaInterno.java
+++ Sistema_Bancario/src/br/com/poo/banco/SistemaInterno.java
@@ -1,9 +1,13 @@
 package br.com.poo.banco;
 
+import br.com.poo.banco.pessoas.Funcionario;
+
 public class SistemaInterno {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Funcionario f = new Funcionario();
+		System.out.println(f);
 
 	}
 
--- Sistema_Bancario/src/br/com/poo/banco/pessoas/Funcionario.java
+++ Sistema_Bancario/src/br/com/poo/banco/pessoas/Funcionario.java
@@ -1,5 +1,32 @@
 package br.com.poo.banco.pessoas;
 
-public class Funcionario {
+public class Funcionario extends Cliente {
+	
+	private double salario;
+	private int id;
+	
+	public Funcionario() {
+		
+	}
+	
+	public Funcionario(double salario, int id) {
+		super();
+		this.salario = salario;
+		this.id = id;
+	}
 
+	public double getSalario() {
+		return salario;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Salário: " + salario + "\nID: " + id;
+	}
+	
+	
 }
