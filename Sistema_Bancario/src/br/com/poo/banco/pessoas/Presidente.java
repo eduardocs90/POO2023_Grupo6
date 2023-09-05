4ec67f8f9ea3748d05dafe2d17ccbbe442ad6a88
LucasGonRo <gonzagal206@gmail.com>
2023-09-05 15:28:47 -0300

    feat: Criação de atributos e gets e setter na classe Presidente.

--- Sistema_Bancario/src/br/com/poo/banco/pessoas/Presidente.java
+++ Sistema_Bancario/src/br/com/poo/banco/pessoas/Presidente.java
@@ -1,5 +1,28 @@
 package br.com.poo.banco.pessoas;
 
-public class Presidente {
+public class Presidente extends Funcionario {
+	
+	private int numDiretor;
+	
+	public Presidente() {	
+	}
+
+	public Presidente(int numDiretor) {
+		super();
+		this.numDiretor = numDiretor;
+	}
+
+	public int getNumDiretor() {
+		return numDiretor;
+	}
+
+	public void setNumDiretor(int numDiretor) {
+		this.numDiretor = numDiretor;
+	}
+
+	@Override
+	public String toString() {
+		return "Números de Diretores gerenciados:" + numDiretor ;
+	}
 
 }
