e0272d4eb5e588d519bffe1250c7688cae8227b3
leignel <cristian.leignel@gmail.com>
2023-09-05 12:40:35 -0300

    feat: Criação dos atributos, getsettrs e construtores da classe Cliente

--- Sistema_Bancario/src/br/com/poo/banco/pessoas/Cliente.java
+++ Sistema_Bancario/src/br/com/poo/banco/pessoas/Cliente.java
@@ -1,5 +1,51 @@
 package br.com.poo.banco.pessoas;
 
 public class Cliente {
+	
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String contato;
+	
+	public Cliente() {
+		
+	}
+	
+	public Cliente(String nome, String cpf, String endereco, String contato) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.contato = contato;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public String getContato() {
+		return contato;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
 
+	@Override
+	public String toString() {
+		return "Nome do cliente: " + nome + "\ncpf: " + cpf + "\nendereco: " + endereco + "\ncontato: " + contato;
+	}
+	
+	
 }
