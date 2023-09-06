2864046a0d2afa863c65de7da52ee026de30c359
leignel <cristian.leignel@gmail.com>
2023-09-06 14:12:55 -0300

    feat: protótipo do chequeEspecial e rendimento

--- Sistema_Bancario/src/br/com/poo/banco/contas/ContaPoupanca.java
+++ Sistema_Bancario/src/br/com/poo/banco/contas/ContaPoupanca.java
@@ -5,7 +5,7 @@ public class ContaPoupanca extends Conta {
 	private double rendimento;
 	
 	public ContaPoupanca() {
-		
+		super();
 	}
 	
 	public ContaPoupanca(String numConta, int numAgencia, String titular, double saldo, double rendimento) {
@@ -16,7 +16,10 @@ public class ContaPoupanca extends Conta {
 	public double getRendimento() {
 		return rendimento;
 	}
-
+	
+	/* rendimentoTotal será o saldo atual da conta (poupança * rendimento (ao mês 0,72%) (ao ano 8,41%), o rendimentoParcial será informado pelo presidente do banco após
+	 as alterações feitas pelo Banco Central */
+	
 	@Override
 	public String toString() {
 		return "Rendimentos : " + rendimento + "Anuais";
