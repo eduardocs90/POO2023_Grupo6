297fed44965f78d9ebf124a9ce364dfce2660e7c
leignel <cristian.leignel@gmail.com>
2023-09-06 13:19:07 -0300

    feat: adição do atributo tarifa na classe ContaCorrente

--- Sistema_Bancario/src/br/com/poo/banco/contas/ContaCorrente.java
+++ Sistema_Bancario/src/br/com/poo/banco/contas/ContaCorrente.java
@@ -3,20 +3,27 @@ package br.com.poo.banco.contas;
 public class ContaCorrente extends Conta {
 
 	private double chequeEspecial;
+	private double tarifa;
 
 	public ContaCorrente() {
 
 	}
 
-	public ContaCorrente(String numConta, int numAgencia, String titular, double saldo, double chequeEspecial) {
+	public ContaCorrente(String numConta, int numAgencia, String titular, double saldo, double chequeEspecial, double tarifa) {
 		super(numConta, numAgencia, titular, saldo);
 		this.chequeEspecial = chequeEspecial;
+		this.tarifa = tarifa;
 	}
 
 	public double getChequeEspecial() {
 		return chequeEspecial;
 	}
-
+	
+	public double getTarifa() {
+		return tarifa;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "Cheque Especial :" + chequeEspecial + "reais";
